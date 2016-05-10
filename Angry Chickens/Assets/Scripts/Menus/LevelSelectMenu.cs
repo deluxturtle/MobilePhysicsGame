fde428b2149655111c521e76edad2dae9c263616
Andrew Seba <sebaturtle@gmail.com>
2016-05-09 20:05:02 -0500

    Quick update before work.

---
+++ Angry Chickens/Assets/Scripts/Menus/LevelSelectMenu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class LevelSelectMenu : MonoBehaviour 
+{
+    public GameObject buttonGroup;
+
+    //Private
+    List<GameObject> levelButtons = new List<GameObject>();
+
+	void Start()
+    {
+        SaveManager saveManager = GameObject.FindObjectOfType<SaveManager>();
+
+        if (buttonGroup == null)
+        {
+            Debug.LogWarning("Button group not assigned in level select panel.");
+        }
+        else
+        {
+            for(int i = 0; i < buttonGroup.transform.childCount; i++)
+            {
+                for(int j = 0; j < buttonGroup.transform.GetChild(i).childCount; j++)
+                {
+                    string btnName = "";
+                    Button btn = buttonGroup.transform.GetChild(i).GetChild(j).GetComponent<Button>();
+                    btnName = btn.gameObject.name;
+
+
+                    //if previous level has a star go ahead and assign button the link to the level.
+                    if (j - 1 > 0 && saveManager.GetLevelStars(buttonGroup.transform.GetChild(i).GetChild(j - 1).name) > 0)
+                    {
+                        btn.onClick.AddListener(() =>
+                        {
+                            LoadLevel(btnName);
+                        });
+                    }
+                    else
+                    {
+                        //Lock the button
+                        if (j != 0)
+                        {
+                            btn.interactable = false;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Loads level with given string parameter.
+    /// Can't check if scene exists with scenemanager in current unity build.
+    /// </summary>
+    /// <param name="pName"></param>
+    public void LoadLevel(string pName)
+    {
+        SceneManager.LoadScene(pName);
+    }
+}
---
+++ Angry Chickens/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveManager : MonoBehaviour {
+
+
+    static bool created = false;
+
+	void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //Do Initialization stuff here.
+
+
+    }
+
+    public int GetLevelStars(string pLevelName)
+    {
+
+        if (PlayerPrefs.HasKey(pLevelName))
+        {
+            return PlayerPrefs.GetInt(pLevelName);
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+
+    /// <summary>
+    /// Saves how many stars you got in the level.
+    /// </summary>
+    /// <param name="pStars"></param>
+    public void SetLevelStars(int pStars)
+    {
+        string currentLvlName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+        PlayerPrefs.SetInt(currentLvlName, pStars);
+    }
+}
